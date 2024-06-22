18207eb9b45bc39f7b12d746528646500701e57f
Mellownyn <sunnyshettigar@gmail.com>
2024-06-22 09:18:30 -0500

    Init home component

--- client/src/app/enrollment/gather-enrollment/gather-enrollment.component.ts
+++ client/src/app/enrollment/gather-enrollment/gather-enrollment.component.ts
@@ -3,7 +3,8 @@ import { Component } from "@angular/core";
 @Component({
     standalone: true,
     selector: 'app-gather-enrollment',
-    templateUrl: './gather-enrollment.component.html'
+    templateUrl: './gather-enrollment.component.html',
+    styleUrl: './gather-enrollment.component.css'
 })
 export class GatherEnrollmentComponent {
 

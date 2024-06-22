5538baf944ba1b905bb6bb9f6f29e5b45e6c6f06
Mellownyn <sunnyshettigar@gmail.com>
2024-06-22 13:28:23 -0500

    Add angular material and new nav component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HomeComponent } from './home/home.component';
+import { NavComponent } from './nav/nav.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

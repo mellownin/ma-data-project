52b4fb417c16854fff299320d6a0105a71ecc7b9
Mellownyn <sunnyshettigar@gmail.com>
2024-06-22 14:07:16 -0500

    Add navigation buttons

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })

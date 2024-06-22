061f878292868c28a1283d0db8b13917049ae2a3
Mellownyn <sunnyshettigar@gmail.com>
2024-06-22 09:19:37 -0500

    Remove test components

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { GatherEnrollmentComponent } from './enrollment/gather-enrollment/gather-enrollment.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, GatherEnrollmentComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

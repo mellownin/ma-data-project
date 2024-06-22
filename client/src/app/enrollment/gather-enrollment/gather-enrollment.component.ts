eaadae37e53a9c636fecffaee5cdfddfcecd4e37
Mellownyn <sunnyshettigar@gmail.com>
2024-06-22 08:47:30 -0500

    Add pieces for first component, gather enrollment

---
+++ client/src/app/enrollment/gather-enrollment/gather-enrollment.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    selector: 'app-gather-enrollment',
+    templateUrl: './gather-enrollment.component.html'
+})
+export class GatherEnrollmentComponent {
+
+}
